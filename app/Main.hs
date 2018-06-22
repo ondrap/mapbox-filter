1c28810beb6bb3b2ccd04285d89b88d4c547e1b0
Ondrej Palkovsky <palkovsky.ondrej@gmail.com>
2018-06-23 00:24:50 +0200

    Pridana zakladni podpora pro S3 publish.

--- app/Main.hs
+++ app/Main.hs
@@ -7,9 +7,9 @@ module Main where
 import           Control.Concurrent.ParallelIO.Global (parallel_,
                                                        stopGlobalPool)
 import           Control.Exception.Safe               (catchAny)
-import           Control.Lens                         ((%~), (&), (^.), (^..),
-                                                       _1)
-import           Control.Monad                        (when)
+import           Control.Lens                         ((%~), (&), (?~), (^.),
+                                                       (^..), _1)
+import           Control.Monad                        (void, when, (>=>))
 import           Control.Monad.IO.Class               (liftIO)
 import           Data.Aeson                           ((.=))
 import qualified Data.Aeson                           as AE
@@ -31,6 +31,15 @@ import           Database.SQLite.Simple               (Connection, Only (..),
 import           Geography.VectorTile                 (layers, linestrings,
                                                        metadata, name, points,
                                                        polygons, tile)
+import           Network.AWS                          (Credentials (Discover),
+                                                       newEnv, runAWS,
+                                                       runResourceT, send,
+                                                       toBody)
+import           Network.AWS.S3                       (BucketName (..),
+                                                       ObjectKey (..),
+                                                       poCacheControl,
+                                                       poContentEncoding,
+                                                       poContentType, putObject)
 import           Options.Applicative                  hiding (header, style)
 import           System.Posix.Files                   (getFileStatus,
                                                        modificationTime)
@@ -39,6 +48,7 @@ import           Text.Read                            (readMaybe)
 import           Web.Scotty                           (addHeader, get, header,
                                                        json, param, raise, raw,
                                                        scotty, setHeader)
+
 
 import           Mapbox.Filters
 import           Mapbox.Interpret                     (FeatureType (..),
@@ -66,6 +76,13 @@ data CmdLine =
     , fLazyUpdate :: Bool
     , fMbtiles    :: FilePath
   }
+  | CmdPublish {
+      fModStyle   :: Maybe FilePath
+    , fSourceName :: Maybe T.Text
+    , fUrlPrefix  :: T.Text
+    , fStoreTgt   :: BucketName
+    , fMbtiles    :: FilePath
+  }
 
 dumpOptions :: Parser CmdLine
 dumpOptions = CmdDump <$> strOption (short 'j' <> long "style" <> help "JSON mapbox style file")
@@ -85,12 +102,27 @@ webOptions = CmdWebServer <$> optional (strOption (short 'j' <> long "style" <> 
                           <*> switch (short 'l' <> long "lazy" <> help "Lazily update the database with filtered data")
                           <*> argument str (metavar "MBTILES" <> help "MBTile SQLite database")
 
+
+s3Bucket :: ReadM BucketName
+s3Bucket = maybeReader s3Reader
+  where
+    s3Reader txt = BucketName . stripRightSlash <$> T.stripPrefix "s3://" (T.pack txt)
+    stripRightSlash txt = fromMaybe txt (T.stripSuffix "/" txt)
+
+publishOptions :: Parser CmdLine
+publishOptions = CmdPublish <$> optional (strOption (short 'j' <> long "style" <> help "JSON mapbox style file"))
+                            <*> optional (T.pack <$> strOption (short 's' <> long "source" <> help "Tile source name"))
+                            <*> (T.pack <$> strOption (short 'u' <> long "url-prefix" <> help "External tile URL prefix"))
+                            <*> option s3Bucket (short 't' <> long "target" <> help "S3 target prefix for files (e.g. s3://my-bucket/map/)")
+                            <*> argument str (metavar "MBTILES" <> help "MBTile SQLite database")
+
 cmdLineParser  :: Parser CmdLine
 cmdLineParser =
   subparser $
     command "dump" (info (helper <*> dumpOptions) (progDesc "Dump vector files contents."))
     <> command "filter" (info (helper <*> mbtileOptions) (progDesc "Run filtering on a MBTiles database"))
     <> command "web" (info (helper <*> webOptions) (progDesc "Run a web server for serving tiles"))
+    <> command "publish" (info (helper <*> publishOptions) (progDesc "Publish mbtile to S3"))
 
 progOpts :: ParserInfo CmdLine
 progOpts = info (cmdLineParser <**> helper)
@@ -121,12 +153,12 @@ checkStyle mtilesrc styl = do
   return $ styl & msLayers %~ filter (\l -> l ^. lSource == tilesrc)
 
 -- | Generate metadata json based on modification text + database + other info
-genMetadata :: Connection -> TL.Text -> TL.Text -> TL.Text -> IO AE.Value
-genMetadata conn minfo proto host = do
+genMetadata :: Connection -> TL.Text -> TL.Text -> IO AE.Value
+genMetadata conn modTimeStr urlPrefix = do
     metalines :: [(T.Text,String)] <- query_ conn "select name,value from metadata"
     return $ AE.object $
         concatMap addMetaLine metalines
-        ++ ["tiles" .= [proto <> "://" <> host <> "/tiles/" <> minfo <> "/{z}/{x}/{y}"],
+        ++ ["tiles" .= [urlPrefix <> "/tiles/" <> modTimeStr <> "/{z}/{x}/{y}"],
             "tilejson" .= ("2.0.0" :: T.Text)
             ]
   where
@@ -171,28 +203,80 @@ dumpPbf style zoom fp = do
       let include = runFilter lfilter ptype feature
       putStrLn $ bool "- " "  " include <> show ptype <> " " <> show (feature ^. metadata)
 
-convertMbtiles :: MapboxStyle -> FilePath -> IO ()
-convertMbtiles style fp =
-  withConnection fp $ \conn -> do
+type JobAction = ((Int, Int, Int, T.Text), BL.ByteString) -> IO ()
+
+-- | Run a filtering action on all tiles in the database and perform a JobAction
+runFilterJob :: Connection -> Maybe MapboxStyle -> JobAction -> IO ()
+runFilterJob conn mstyle saveAction = do
     zlevels <- query_ conn "select distinct zoom_level from map order by zoom_level"
     for_ zlevels $ \(Only zoom) -> do
       putStrLn $ "Filtering zoom: " <> show zoom
-      let filtList = styleToCFilters zoom style
 
-      (tiles :: [Only T.Text]) <- query conn "select tile_id from map where zoom_level=?" (Only zoom)
+      (tiles :: [(Int,Int,Int,T.Text)]) <- query conn "select zoom_level,tile_column,tile_row,tile_id from map where zoom_level=?" (Only zoom)
       putStrLn $ "Tiles: " <> show (length tiles)
-      parallel_ $ shrinkTile conn filtList <$> tiles
-      -- sequence_ $ (shrinkTile conn filtList) <$> tiles
+      let iaction = case mstyle of
+            Just style -> shrinkTile (styleToCFilters zoom style)
+            Nothing    -> saveAction
+      parallel_ $ (fetchTile >=> iaction) <$> tiles
     stopGlobalPool
-    execute_ conn "vacuum"
+    -- If we were shrinking, call vacuum on database, otherwise skip it
+    for_ mstyle $ \_ -> execute_ conn "vacuum"
   where
-    shrinkTile conn filtList (Only tileid) = do
+    fetchTile tid@(_,_,_,tileid) = do
       [Only (tdata :: BL.ByteString)] <- query conn "select tile_data from images where tile_id=?" (Only tileid)
+      return (tid, tdata)
+
+    shrinkTile filtList (tid@(_,_,_,tileid), tdata) =
       case filterTileCs filtList tdata of
         Left err -> putStrLn $ "Error when decoding tile " <> show tileid <> ": " <> cs err
-        Right newdta ->
-          execute conn "update images set tile_data=? where tile_id=?" (newdta, tileid)
+        Right newdta -> saveAction (tid, newdta)
 
+-- | Filter all tiles in a database and save the filtered tiles back
+convertMbtiles :: MapboxStyle -> FilePath -> IO ()
+convertMbtiles style mbtiles =
+  withConnection mbtiles $ \conn ->
+    runFilterJob conn (Just style) $ \((_,_,_,tileid), newdta) ->
+      execute conn "update images set tile_data=? where tile_id=?" (newdta, tileid)
+
+-- | Publish the mbtile to an S3 target, make it ready for serving
+runPublishJob :: Maybe (MapboxStyle, EpochTime) -> FilePath -> T.Text -> BucketName -> IO ()
+runPublishJob mstyle mbtiles urlPrefix storeTgt = do
+  env <- newEnv Discover
+  withConnection mbtiles $ \conn -> do
+    modstr <- liftIO $ makeModtimeStr conn (snd <$> mstyle)
+    runFilterJob conn (fst <$> mstyle) $ \((z,x,y,_), newdta) -> do
+      let xyz_y = yzFlipTms y z
+          dstpath = ObjectKey (cs ("tiles/" <> modstr <> "/" <> show z <> "/" <> show x <> "/" <> show xyz_y))
+      runResourceT $ runAWS env $ do
+        let cmd = putObject storeTgt dstpath (toBody newdta)
+                  & poContentType ?~ "application/x-protobuf"
+                  & poContentEncoding ?~ "gzip"
+                  & poCacheControl ?~ "max-age=31536000"
+        void $ send cmd
+    meta <- genMetadata conn (cs modstr) (cs urlPrefix)
+    let cmd = putObject storeTgt "metadata.json" (toBody (AE.encode meta))
+              & poContentType ?~ "application/json"
+    runResourceT $ runAWS env $
+      void (send cmd)
+
+-- | Flip y coordinate between xyz and tms schemes
+yzFlipTms :: Int -> Int -> Int
+yzFlipTms y z = 2 ^ z - y - 1
+
+-- | Make a string containing modification time of db & style
+makeModtimeStr :: Connection -> Maybe EpochTime -> IO String
+makeModtimeStr conn mtime = do
+    dbmtime <- liftIO getDbMtime
+    let stmtime = fromMaybe 0 mtime
+    return (dbmtime <> "_" <> show stmtime)
+  where
+    getDbMtime = do
+      mlines :: [Only String] <- query_ conn "select value from metadata where name='mtime'"
+      case mlines of
+        [Only res] -> return res
+        _          -> return ""
+
+-- | Run a web server serving filtered/unfiltered tiles and metadata
 runWebServer :: Int -> Maybe (MapboxStyle, EpochTime) -> FilePath -> Bool -> IO ()
 runWebServer port mstyle mbpath lazyUpdate =
   withConnection mbpath $ \conn -> do
@@ -207,17 +291,15 @@ runWebServer port mstyle mbpath lazyUpdate =
           -- find out protocol and host
           proto <- fromMaybe "http" <$> header "X-Forwarded-Proto"
           host <- fromMaybe "localhost" <$> header "Host"
-          dbmtime <- liftIO $ getDbMtime conn
-          let stmtime = maybe 0 snd mstyle
-          let mtime = dbmtime <> "_" <> show stmtime
-          metaJson <- liftIO $ genMetadata conn (cs mtime) proto host
+          mtime <- liftIO $ makeModtimeStr conn (snd <$> mstyle)
+          metaJson <- liftIO $ genMetadata conn (cs mtime) (proto <> "://" <> host)
           addHeader "Access-Control-Allow-Origin" "*"
           json metaJson
       get "/tiles/:mt1/:z/:x/:y" $ do
           z :: Int <- param "z"
           x :: Int <- param "x"
           y :: Int <- param "y"
-          let tms_y = 2 ^ z - y - 1
+          let tms_y = yzFlipTms y z
 
           mnewtile <- case (mstyle, lazyUpdate) of
               (Just (style,_), True) -> getLazyTile conn style z x tms_y
@@ -225,7 +307,7 @@ runWebServer port mstyle mbpath lazyUpdate =
 
           addHeader "Access-Control-Allow-Origin" "*"
           setHeader "Content-Type" "application/x-protobuf"
-          setHeader "Cache-control" "max-age=31536000"
+          setHeader "Cache-Control" "max-age=31536000"
 
           case mnewtile of
             Just dta -> do
@@ -233,12 +315,6 @@ runWebServer port mstyle mbpath lazyUpdate =
               raw dta
             Nothing -> raw ""
   where
-    getDbMtime conn = do
-      mlines :: [Only String] <- query_ conn "select value from metadata where name='mtime'"
-      case mlines of
-        [Only res] -> return res
-        _          -> return ""
-
     -- Get tile from modified database; if not already shrinked, shrink it and write to the database
     getLazyTile conn style z x tms_y = do
       rows <- liftIO $ query conn "select tile_data, map.tile_id, shrinked from images,map where zoom_level=? AND tile_column=? AND tile_row=? AND map.tile_id=images.tile_id"
@@ -271,21 +347,25 @@ runWebServer port mstyle mbpath lazyUpdate =
 main :: IO ()
 main = do
   opts <- execParser progOpts
-  let tilesrc = fSourceName opts
 
   case opts of
-    CmdDump{fMvtSource, fZoomLevel, fStyle} -> do
-        style <- getStyle fStyle >>= checkStyle tilesrc
+    CmdDump{fMvtSource, fZoomLevel, fStyle, fSourceName} -> do
+        style <- getStyle fStyle >>= checkStyle fSourceName
         dumpPbf style fZoomLevel fMvtSource
-    CmdMbtiles{fMbtiles, fStyle} -> do
-        style <- getStyle fStyle >>= checkStyle tilesrc
+    CmdMbtiles{fMbtiles, fStyle, fSourceName} -> do
+        style <- getStyle fStyle >>= checkStyle fSourceName
         convertMbtiles style fMbtiles
-    CmdWebServer{fModStyle, fWebPort, fMbtiles, fLazyUpdate} -> do
-        mstyle <- case fModStyle of
-            Nothing -> return Nothing
-            Just fp -> do
-              st <- getStyle fp  >>= checkStyle tilesrc
-              fstat <- getFileStatus fp
-              return (Just (st, modificationTime fstat))
+    CmdWebServer{fModStyle, fWebPort, fMbtiles, fLazyUpdate, fSourceName} -> do
+        mstyle <- getMStyle fModStyle fSourceName
         runWebServer fWebPort mstyle fMbtiles fLazyUpdate
-
+    CmdPublish{fModStyle, fSourceName, fUrlPrefix, fStoreTgt, fMbtiles } -> do
+        mstyle <- getMStyle fModStyle fSourceName
+        runPublishJob mstyle fMbtiles fUrlPrefix fStoreTgt
+  where
+    getMStyle stname tilesrc =
+      case stname of
+        Nothing -> return Nothing
+        Just fp -> do
+          st <- getStyle fp  >>= checkStyle tilesrc
+          fstat <- getFileStatus fp
+          return (Just (st, modificationTime fstat))
